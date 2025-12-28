b37df24aa2e3664741830b1ae9e8c646070e2f56
Idrak <iatakishi@gmail.com>
2025-12-28 19:46:11 +0000

    2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    count = len(args)
+    if count == 0:
+        print("0 arguments.")
+    else:
+        print("{} argument{}:".format(count, "" if count == 1 else "s"))
+        for i, arg in enumerate(args, start=1):
+            print("{}: {}".format(i, arg))
+
