6310a8e21c08c8f9964a4f5f8252c6994f6fab30
Idrak <iatakishi@gmail.com>
2026-01-05 02:16:25 +0400

    0 fixed

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -2,4 +2,4 @@
 def print_list_integer(my_list=[]):
     """Print all integers in a list, one per line."""
     for num in my_list:
-        print("{}".format(num))
+        print("{:d}".format(num))
