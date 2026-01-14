34a62c4bf7c2b5194d4c96cdfeee050a2dbed839
Idrak <iatakishi@gmail.com>
2026-01-15 00:18:14 +0400

    8 added

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+def uppercase(str):
+    for i in str:
+        if ord(i) >= 97:
+            print(chr(ord(i) - 32), end = "")
+        else:
+            print(i, end = "")
+    print()
