3709a152dabed453fd220209b094092fb79551a6
Idrak <iatakishi@gmail.com>
2026-01-18 00:08:47 +0400

    4 added

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if 0 < idx < len(my_list)+1:
-        my_list[idx] = element
+    new_list = my_list.copy()
+    if 0 <= idx < len(my_list)+1:
+        new_list[idx] = element
     return my_list
