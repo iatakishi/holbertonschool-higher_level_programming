7b2c0b5c7b0342c38ad203e98a8df4d2d03603af
Idrak <iatakishi@gmail.com>
2026-01-18 20:22:17 +0400

    9 added

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
-    max_num = 0
+    max_num = my_list[0]
     for i in my_list:
         if i > max_num:
             max_num = i
