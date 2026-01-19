d9f7269752bfe5c5cf94199c834a526bb4d9fa33
Idrak <iatakishi@gmail.com>
2026-01-19 23:01:55 +0400

    10 added

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -7,4 +7,6 @@ def best_score(a_dictionary):
             if a_dictionary[key] > best_value:
                 best_value = a_dictionary[key]
                 best_key = key
+    else:
+        best_key = None
     return best_key
