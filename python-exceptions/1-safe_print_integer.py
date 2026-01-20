44647cb1b0761b5b8b40561dba4c1e971db3803b
Idrak <iatakishi@gmail.com>
2026-01-20 16:00:56 +0400

    1 added

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -3,6 +3,6 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError:
+    except TypeError, ValueError:
         print("{} is not an integer".format(value))
         return False
