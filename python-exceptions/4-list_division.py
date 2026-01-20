d036de1e4bf644ed3e41c45090df3561145be36c
Idrak <iatakishi@gmail.com>
2026-01-20 19:51:39 +0400

    4 added

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
-    div_result = 0
+    div_result = 0.0
     for i in range(list_length):
         try:
             div_result = my_list_1[i] / my_list_2[i]
