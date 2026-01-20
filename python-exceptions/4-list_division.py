0d97fc9b5dc21a2291619ac5a4a1fb7803df19ee
Idrak <iatakishi@gmail.com>
2026-01-20 19:53:55 +0400

    4 added

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
-    div_result = 0.0
     for i in range(list_length):
+        div_result = 0
         try:
             div_result = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError: print("division by 0")
