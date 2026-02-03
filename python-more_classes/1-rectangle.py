ac1b6715f07eb269c997f21bcbf02afdb328a085
Idrak <iatakishi@gmail.com>
2026-02-04 00:21:07 +0400

    1 added

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""This modules defines rectangle class."""
+
+
+class Rectangle:
+    """This class defines a rectangle."""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    """For width"""
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    """For height"""
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
