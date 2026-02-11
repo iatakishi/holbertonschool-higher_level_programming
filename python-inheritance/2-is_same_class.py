8d0d4d06839c126062a1f4cd873f9808f86e460a
Idrak <iatakishi@gmail.com>
2026-02-11 23:37:46 +0400

    2 added

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+"""This module designs a is_same_class function."""
+
+
 def is_same_class(obj, a_class):
+    """This function checks if an object is a type of class."""
     return type(obj) is a_class
