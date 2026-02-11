b31fb8693fc8ae7a824496c6cf6573bc04e58544
Idrak <iatakishi@gmail.com>
2026-02-11 23:48:33 +0400

    3 added

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module defines is_kind_of_class function."""
+
+
+def is_kind_of_class(obj, a_class):
+    """This function defines the class of an object."""
+    return isinstance(obj, a_class)
