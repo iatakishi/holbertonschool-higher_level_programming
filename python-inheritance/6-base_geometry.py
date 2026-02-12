cdafdb2518d140ccab1dfa4b39b9972003f2443c
Idrak <iatakishi@gmail.com>
2026-02-12 20:44:07 +0400

    6 added

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module defines a class called BaseGeometry."""
+
+
+class BaseGeometry:
+    """This class defines BaseGeometry."""
+    def area(self):
+        raise Exception("area() is not implemented")
