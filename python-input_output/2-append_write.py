fd4ab8a13864ae98acf1abb304f339e52c89c384
Idrak <iatakishi@gmail.com>
2026-02-13 23:32:46 +0400

    2 added

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -5,4 +5,4 @@
 def append_write(filename="", text=""):
     """This function appends a text."""
     with open(filename, "a", encoding="utf-8") as f:
-        f.write(text)
+        return f.write(text)
