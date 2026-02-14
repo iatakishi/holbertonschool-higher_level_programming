e5651315ce3a6d00d22313354092c15a20703366
Idrak <iatakishi@gmail.com>
2026-02-14 19:33:28 +0400

    10 added

---
+++ python-input_output/10-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module defines student class."""
+
+
+class Student:
+    """This class defines a student object."""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is not None and isinstance(attrs, list):
+            return {key: self.__dict__[key] for key in attrs if key in self.__dict__}
+        return self.__dict__
