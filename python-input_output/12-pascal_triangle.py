c9b784faeab06f19050be0efb04f7f74ddba1a2e
Idrak <iatakishi@gmail.com>
2026-02-15 19:31:48 +0400

    12 added

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Returns Pascal's triangle of n rows."""
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]  # first row
+
+    for i in range(1, n):
+        prev = triangle[-1]
+        row = [1]  # start with 1
+
+        for j in range(1, len(prev)):
+            row.append(prev[j - 1] + prev[j])  # sum of two parents
+
+        row.append(1)  # end with 1
+        triangle.append(row)
+
+    return triangle
