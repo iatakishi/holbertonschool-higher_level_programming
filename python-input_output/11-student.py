5f4c0f25d050545164e83c54f17b507b61182adb
Idrak <iatakishi@gmail.com>
2026-02-15 13:46:27 +0400

    11 added

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -17,6 +17,9 @@ class Student:
         return self.__dict__
 
     def reload_from_json(self, json):
-        self.first_name = json["first_name"]
-        self.last_name = json["last_name"]
-        self.age = json["age"]
+        if "first_name" in json:
+            self.first_name = json["first_name"]
+        if "last_name" in json:
+            self.last_name = json["last_name"]
+        if "age" in json:
+            self.age = json["age"]
