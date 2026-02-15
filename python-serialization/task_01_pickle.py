c39bc37775721888cd7d61c5c225ffc1ec3c7613
Idrak <iatakishi@gmail.com>
2026-02-15 21:47:25 +0400

    01 added

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""This module builds CustomObject class."""
+
+import pickle
+
+
+class CustomObject:
+    """This is CustomObject class."""
+
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        print("Name: {}\nAge: {}\nIs Student: {}".format(self.name, self.age, self.is_student))
+
+    def serialize(self, filename):
+        try:
+            with open(filename, "wb") as f:
+                pickle.dump(self, f)
+        except (FileNotFoundError, pickle.PicklingError):
+            return None
+
+    @classmethod
+    def deserialize(cls, filename):
+        try:
+            with open(filename, "rb") as f:
+                return pickle.load(f)
+        except (FileNotFoundError, pickle.UnpicklingError):
+            return None
