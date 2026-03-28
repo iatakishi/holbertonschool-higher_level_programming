4dde33fc431fb6e23797d6abc5c5972bcf58a2bd
Idrak <iatakishi@gmail.com>
2026-03-28 12:57:14 +0400

    1 added

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 -- creating a user
-CREATE USER 'user_0d_1' @ 'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1' @ 'localhost' WITH GRANT OPTION;
+DROP USER IF EXISTS 'user_0d_1'@'localhost';
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
